8eec7e18afe005172317a632b2f8f187ac837593
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-10-11 00:18:15 +0900

    First Commit : project set up

---
+++ android/app/src/main/kotlin/com/example/block_breaker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.block_breaker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
