c0a6720010089ccbe8bbc2df0da6c8ff02f52490
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-10-11 00:34:33 +0900

    add game "block_breaker"

---
+++ lib/game/block_breaker.dart
@@ -0,0 +1,6 @@
+import 'package:flame/game.dart';
+
+class BlockBreaker extends FlameGame {
+  @override
+  Future<void> onLoad() async {}
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'game/block_breaker.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyWidget(),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  final game = BlockBreaker();
+  runApp(GameWidget(game: game));
 }
